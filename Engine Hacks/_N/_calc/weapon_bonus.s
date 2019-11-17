a699dc54e3400f99e82faa5a85fd750923e64499
aera <eliwooood@gmail.com>
2019-11-17 22:59:03 +0900

    武器レベルS効果 攻撃+1、命中+10

--- Engine Hacks/_N/_calc/weapon_bonus.s
+++ Engine Hacks/_N/_calc/weapon_bonus.s
@@ -16,7 +16,6 @@ LEVEL_S = (6)
 
 @0802acc8
 .thumb
-	b end
 	mov r0, #0x50
 	ldrb r0, [r4, r0]
 	cmp r0, #SWORD
@@ -36,101 +35,8 @@ LEVEL_S = (6)
 	cmp r0, #DARK
 	beq typeC
 	b end
-@剣・杖・光
 typeA:
-	add	r0, r4
-	add	r0, #40
-	ldrb	r0, [r0]
-	bl Get_WpLv
-	mov r3, r0
-	mov r0, #0
-	mov r1, #0
-	mov r2, #0
-	
-	cmp r3, #LEVEL_E
-	blt merge
-	mov r0, #5
-	mov r1, #7
-	mov r2, #5
-	
-	cmp r3, #LEVEL_D
-	blt merge
-	mov r0, #6
-	mov r1, #10
-	mov r2, #6
-	
-	cmp r3, #LEVEL_C
-	blt merge
-	mov r0, #7
-	mov r1, #13
-	mov r2, #7
-	
-	cmp r3, #LEVEL_B
-	blt merge
-	mov r0, #8
-	mov r1, #16
-	mov r2, #8
-	
-	cmp r3, #LEVEL_A
-	blt merge
-	mov r0, #10
-	mov r1, #20
-	mov r2, #10
-	
-	cmp r3, #LEVEL_S
-	blt merge
-	nop
-	b merge
-	
-	
-@槍・弓
 typeB:
-	add	r0, r4
-	add	r0, #40
-	ldrb	r0, [r0]
-	bl Get_WpLv
-	mov r3, r0
-	mov r0, #0
-	mov r1, #0
-	mov r2, #0
-	
-	cmp r3, #LEVEL_E
-	blt merge
-	mov r0, #6
-	mov r1, #6
-	mov r2, #5
-	
-	cmp r3, #LEVEL_D
-	blt merge
-	mov r0, #8
-	mov r1, #8
-	mov r2, #6
-	
-	cmp r3, #LEVEL_C
-	blt merge
-	mov r0, #10
-	mov r1, #10
-	mov r2, #7
-	
-	cmp r3, #LEVEL_B
-	blt merge
-	mov r0, #12
-	mov r1, #12
-	mov r2, #8
-	
-	cmp r3, #LEVEL_A
-	blt merge
-	mov r0, #15
-	mov r1, #15
-	mov r2, #10
-	
-	cmp r3, #LEVEL_S
-	blt merge
-	nop
-	b merge
-	
-	
-@斧・理・闇
 typeC:
 	add	r0, r4
 	add	r0, #40
@@ -141,55 +47,18 @@ typeC:
 	mov r1, #0
 	mov r2, #0
 	
-	cmp r3, #LEVEL_E
-	blt merge
-	mov r0, #7
-	mov r1, #5
-	mov r2, #5
-	
-	cmp r3, #LEVEL_D
-	blt merge
-	mov r0, #10
-	mov r1, #6
-	mov r2, #6
-	
-	cmp r3, #LEVEL_C
-	blt merge
-	mov r0, #13
-	mov r1, #7
-	mov r2, #7
-	
-	cmp r3, #LEVEL_B
-	blt merge
-	mov r0, #16
-	mov r1, #8
-	mov r2, #8
-	
-	cmp r3, #LEVEL_A
-	blt merge
-	mov r0, #20
-	mov r1, #10
-	mov r2, #10
-	
 	cmp r3, #LEVEL_S
-	blt merge
-	nop
-	b merge
-merge:
-	add r4, #96
-	ldrh r3, [r4]
-	add r3, r0
-	strh r3, [r4]
+	blt end
 	
-	add r4, #2
-	ldrh r3, [r4]
-	add r3, r1
-	strh r3, [r4]
+	mov r1, #90
+	ldrh r0, [r4, r1]
+	add r0, #1
+	strh r0, [r4, r1] @自分
+	mov r1, #96
+	ldrh r0, [r4, r1]
+	add r0, #10
+	strh r0, [r4, r1] @自分
 	
-	add r4, #6
-	ldrh r3, [r4]
-	add r3, r2
-	strh r3, [r4]
 end:
 	pop	{r4}
 	pop	{r0}
