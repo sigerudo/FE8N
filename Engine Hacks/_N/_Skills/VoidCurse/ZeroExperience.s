43d4592c342ddd12e76598127524c2793406c72c
aera <eliwooood@gmail.com>
2020-02-24 20:59:54 +0900

    捕獲攻撃中は経験値0

--- Engine Hacks/_N/_Skills/VoidCurse/ZeroExperience.s
+++ Engine Hacks/_N/_Skills/VoidCurse/ZeroExperience.s
@@ -12,6 +12,18 @@ false:
 
 main:
         push {r2, lr}
+
+        ldrb r0, [r3, #27]
+        cmp r0, #0
+        bne next    @誰か同行しているなら通常
+        ldr r0, [r3, #12]
+        mov r1, #0x10
+        tst r0, r1
+        beq next    @救出中でなければ通常
+        mov r0, #1
+        pop {r2, pc}
+    next:
+
         mov r0, r3
         mov r1, #0
         bl HAS_VOID_CURSE
